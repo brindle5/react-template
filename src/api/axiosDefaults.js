7a3930343ed3ba13d291a3ce16c5ce3ce221c2e5
brindle5 <sarah.brindle@hotmail.co.uk>
2023-09-03 14:55:45 +0000

    Create clickoutsidetoggle file

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://drf-project-293f81595c35.herokuapp.com/'
-axios.defaults.headers.post['Content Type'] = 'mulitpart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'https://drf-project-293f81595c35.herokuapp.com/';
+axios.defaults.headers.post['Content Type'] = 'mulitpart/form-data';
+axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
