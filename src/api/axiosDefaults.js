237dd43f51fe09dc43205afd5f0f08011002d9f0
brindle5 <sarah.brindle@hotmail.co.uk>
2023-09-03 16:20:02 +0000

    Fix errors in code

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'https://drf-project-293f81595c35.herokuapp.com/';
-axios.defaults.headers.post['Content Type'] = 'mulitpart/form-data';
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
